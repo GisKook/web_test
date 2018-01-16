544c286e4c8eaea7f9e4ade0c3eae694a184e8d0
GisKook <zhangkai.gis@163.com>
2018-01-16 13:16:25 +0800

    add press test

--- entity/entity.go
+++ entity/entity.go
@@ -5,12 +5,38 @@ import (
 	"io/ioutil"
 	"log"
 	"time"
+	"fmt"
+	"sync"
+	"math/rand"
 
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/client"
 )
 
-func main() {
+const(
+	DST_REMOTE_ADDR_FMT string = "http://222.222.218.50:%s/json"
+	DST_SCREEN_SHOT_FMT string = "%s.png"
+)
+
+func main(){
+	var wg sync.WaitGroup
+
+	ports := []string{"9222", "9223"}
+	for _,port:= range ports{
+		log.Println("--------------chrome port-------------",port)
+		wg.Add(1)
+		p := port
+		go func(){ 
+			dst_remote_addr := fmt.Sprintf(DST_REMOTE_ADDR_FMT, p)
+			dst_screen_shot := fmt.Sprintf(DST_SCREEN_SHOT_FMT, p)
+			attach_chrome(dst_remote_addr, dst_screen_shot)
+		}()
+	}
+	wg.Wait()
+}
+
+func attach_chrome(remote_addr, file_name string) {
+	log.Println(remote_addr, file_name)
 	var err error
 
 	// create context
@@ -18,20 +44,25 @@ func main() {
 	defer cancel()
 
 	// create chrome instance
-	c, err := chromedp.New(ctxt, chromedp.WithTargets(client.New().WatchPageTargets(ctxt)),chromedp.WithLog(log.Printf))
+	c, err := chromedp.New(ctxt, chromedp.WithTargets(client.New(client.URL(remote_addr)).WatchPageTargets(ctxt)),chromedp.WithLog(log.Printf))
 	// c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	for{
 	// run task list
 	var buf []byte
 	err = c.Run(ctxt, screenshot(`http://222.222.218.52:8023/web/user/main`, `#map_layer0`, &buf))
 	if err != nil {
-		log.Println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY ....")
-		log.Fatal(err)
+		log.Println(err)
 	}
-	log.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ....")
+
+	err = ioutil.WriteFile(file_name, buf, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 	// shutdown chrome
 	err = c.Shutdown(ctxt)
@@ -45,26 +76,129 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile("contact-form.png", buf, 0644)
-	if err != nil {
-		log.Fatal(err)
+}
+
+type action struct{
+	used int; 
+	value int;
+}
+func gen_action(lm,rm, um, dm int) []chromedp.Action{ 
+	rand.Seed(time.Now().Unix())
+	la := rand.Intn(lm)
+	ra := rand.Intn(rm)
+	ua := rand.Intn(um)
+	da := rand.Intn(dm) 
+	t := la+ra+ua+da
+	actions := make([]*action, t)
+	tt := []int{la, ra, ua, da}
+	for i, va := range tt{
+		v := 0
+		log.Println(va)
+		for v < va{
+			d:=rand.Intn(t)
+			if actions[d] == nil{
+				actions[d] = new(action)
+				actions[d].used = 1 
+				actions[d].value = i // 0 for left 1 right 2 up 3 down
+				v++
+			}
+		}
 	}
+	a := make([]chromedp.Action,0)
+	for _, v := range actions{ 
+		switch v.value{
+		case 0:
+			a = append(a,chromedp.Click(`#left_click`))
+			a = append(a,chromedp.Sleep(1000*time.Millisecond))
+		case 1:
+			a = append(a,chromedp.Click(`#right_click`))
+			a = append(a,chromedp.Sleep(1000*time.Millisecond))
+		case 2:
+			a = append(a,chromedp.Click(`#up_click`))
+			a = append(a,chromedp.Sleep(1000*time.Millisecond))
+		case 3:
+			a = append(a,chromedp.Click(`#down_click`))
+			a = append(a,chromedp.Sleep(1000*time.Millisecond))
+		}
+	}
+
+	return a
 }
 
 func screenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
-	log.Println("------------------------------------")
 	log.Println(urlstr)
 	log.Println(sel)
-
-	return chromedp.Tasks{
-	//	chromedp.Navigate(`http://127.0.0.1:8080/web/user/main`),
-	//	chromedp.WaitVisible(`#map_layer0`),
-//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+	tasks := chromedp.Tasks{
 		chromedp.Navigate(urlstr),
 		chromedp.WaitVisible(sel, chromedp.ByID),
-		chromedp.Sleep(2 * time.Second),
-		//chromedp.WaitVisible(`#map_layer0`),
-	//	chromedp.WaitNotVisible(`div.v-middle > div.la-ball-clip-rotate`, chromedp.ByQuery),
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(1000 * time.Millisecond),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(1000 * time.Millisecond),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(1000 * time.Millisecond),
 	}
+	// a := gen_action(12,12,29,29)
+	a := gen_action(4,4,4,4)
+	tasks = append(tasks, a...)
+	tasks = append(tasks, chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID))
+	tasks = append(tasks, chromedp.Sleep(1000 * time.Millisecond))
+	log.Println("task list ---------------", len(tasks))
+
+	return tasks
+	
+
+//	return chromedp.Tasks{
+//		chromedp.Navigate(urlstr),
+//		chromedp.WaitVisible(sel, chromedp.ByID),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(100 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(100 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(100 * time.Millisecond),
+//		chromedp.Click(`#left_click`),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+//	}
 }
--- main/main.go
+++ main/main.go
@@ -1,29 +1,30 @@
-//// Command click is a chromedp example demonstrating how to use a selector to
-//// click on an element.
-//package main
-//
-//import (
-//	"context"
-//	"log"
-//	"time"
-//
-//	"github.com/chromedp/chromedp"
-//	cdpclient "github.com/chromedp/chromedp/client"
-//)
-//
-//func main() {
-//	var err error
-//
-//	// create context
-//	ctxt, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	// create chrome instance
-//	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	// test
+// Command click is a chromedp example demonstrating how to use a selector to
+// click on an element.
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	"math/rand"
+
+	"github.com/chromedp/chromedp"
+	// cdpclient "github.com/chromedp/chromedp/client"
+)
+
+func main() {
+	var err error
+
+	// create context
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create chrome instance
+	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// test
 //	for i:=0 ; i<0; i++ {
 //	client := cdpclient.New()
 //t, err := client.NewPageTargetWithURL(ctxt, "http://127.0.0.1:8080/web/user/main")
@@ -41,43 +42,146 @@
 //}
 //click().Do(ctxt,h)
 //	}
-//	// test
-//
-//	// run task list
-//	err = c.Run(ctxt, click())
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// shutdown chrome
-//	err = c.Shutdown(ctxt)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// wait for chrome to finish
-//	err = c.Wait()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//}
-//
+	// test
+
+	// run task list
+	for{
+	err = c.Run(ctxt, click())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+	// shutdown chrome
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for chrome to finish
+	err = c.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+type action struct{
+	used int; 
+	value int;
+}
+func gen_action(lm,rm, um, dm int) []chromedp.Action{ 
+	rand.Seed(time.Now().Unix())
+	la := rand.Intn(lm)
+	ra := rand.Intn(rm)
+	ua := rand.Intn(um)
+	da := rand.Intn(dm) 
+	t := la+ra+ua+da
+	actions := make([]*action, t)
+	tt := []int{la, ra, ua, da}
+	for i, va := range tt{
+		v := 0
+		log.Println(va)
+		for v < va{
+			d:=rand.Intn(t)
+			if actions[d] == nil{
+				actions[d] = new(action)
+				actions[d].used = 1 
+				actions[d].value = i // 0 for left 1 right 2 up 3 down
+				v++
+			}
+		}
+	}
+	a := make([]chromedp.Action,0)
+	for _, v := range actions{ 
+		switch v.value{
+		case 0:
+			a = append(a,chromedp.Click(`#left_click`))
+			a = append(a,chromedp.Sleep(500*time.Millisecond))
+		case 1:
+			a = append(a,chromedp.Click(`#right_click`))
+			a = append(a,chromedp.Sleep(500*time.Millisecond))
+		case 2:
+			a = append(a,chromedp.Click(`#up_click`))
+			a = append(a,chromedp.Sleep(500*time.Millisecond))
+		case 3:
+			a = append(a,chromedp.Click(`#down_click`))
+			a = append(a,chromedp.Sleep(500*time.Millisecond))
+		}
+	}
+
+	return a
+}
+
+func click() chromedp.Tasks {
+	tasks := chromedp.Tasks{
+		chromedp.Navigate(`http://127.0.0.1:8080/web/user/main`),
+		chromedp.WaitVisible(`#map_layer0`),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(100 * time.Millisecond),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(100 * time.Millisecond),
+		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
+		chromedp.Sleep(100 * time.Millisecond),
+	}
+	a := gen_action(12,12,29,29)
+	tasks = append(tasks, a...)
+
+	return tasks
+}
+
 //func click() chromedp.Tasks {
 //	return chromedp.Tasks{
 //		chromedp.Navigate(`http://127.0.0.1:8080/web/user/main`),
 //		chromedp.WaitVisible(`#map_layer0`),
 //		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(5 * time.Second),
-//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(5 * time.Second),
+//		chromedp.Sleep(1000 * time.Millisecond),
 //		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(5 * time.Second),
-//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(5 * time.Second),
+//		chromedp.Sleep(1000 * time.Millisecond),
 //		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderIncrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(5 * time.Second),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#up_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#right_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#down_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#left_click`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
 //		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
-//		chromedp.Sleep(300 * time.Second),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
+//		chromedp.Click(`#map_zoom_slider > div.esriSimpleSliderDecrementButton`, chromedp.NodeVisible),
+//		chromedp.Sleep(1000 * time.Millisecond),
 //	}
 //}
 ///////////////////////////////////////////////////////example2////////////////////////////////////////////////////////
@@ -151,95 +255,3 @@
 //		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
 //	}
 //}
-///////////////////////////////////////////////////////example3////////////////////////////////////////////////////////
-package main
-
-import (
-	"context"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"sync"
-	"time"
-
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/cdproto/page"
-	"github.com/chromedp/chromedp"
-	"github.com/chromedp/chromedp/runner"
-)
-
-func main() {
-	var err error
-
-	// create context
-	ctxt, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// create pool
-	pool, err := chromedp.NewPool( chromedp.PortRange(9223,9223)/*chromedp.PoolLog(log.Printf, log.Printf, log.Printf)*/ )
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// loop over the URLs
-	var wg sync.WaitGroup
-	for i, urlstr := range []string{
-		"http://222.222.218.52:8023/web/user/main",
-	} {
-		wg.Add(1)
-		go takeScreenshot(ctxt, &wg, pool, i, urlstr)
-	}
-
-	// wait for to finish
-	wg.Wait()
-
-	// shutdown pool
-	err = pool.Shutdown()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func takeScreenshot(ctxt context.Context, wg *sync.WaitGroup, pool *chromedp.Pool, id int, urlstr string) {
-	defer wg.Done()
-
-	// allocate
-	c, err := pool.Allocate(ctxt,runner.HeadlessPathPort("/headless_shell/headless_shell", 9223))
-	if err != nil {
-		log.Printf("url (%d) `%s` error: %v", id, urlstr, err)
-		return
-	}
-	defer c.Release()
-
-	// run tasks
-	var buf []byte
-	err = c.Run(ctxt, screenshot(urlstr, &buf))
-	if err != nil {
-		log.Printf("url (%d) `%s` error: %v", id, urlstr, err)
-		return
-	}
-
-	// write to disk
-	err = ioutil.WriteFile(fmt.Sprintf("%d.png", id), buf, 0644)
-	if err != nil {
-		log.Printf("url (%d) `%s` error: %v", id, urlstr, err)
-		return
-	}
-}
-
-func screenshot(urlstr string, picbuf *[]byte) chromedp.Action {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.Sleep(2 * time.Second),
-		chromedp.WaitVisible(`#map_layer0`),
-//		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
-		chromedp.ActionFunc(func(ctxt context.Context, h cdp.Executor) error {
-			buf, err := page.CaptureScreenshot().Do(ctxt, h)
-			if err != nil {
-				return err
-			}
-			*picbuf = buf
-			return nil
-		}),
-	}
-}
